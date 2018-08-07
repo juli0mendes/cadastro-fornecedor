0fc61e4d357aad8866a3f3fcc59572a87a05835c
Júlio Cesar Mendes <jcmorais@brasilcap.com.br>
2018-08-07 18:03:01 -0300

    passo 7 ok.

--- src/main/java/br/com/juliomendes90/cadastrofornecedor/controllers/FornecedorController.java
+++ src/main/java/br/com/juliomendes90/cadastrofornecedor/controllers/FornecedorController.java
@@ -1,18 +1,39 @@
 package br.com.juliomendes90.cadastrofornecedor.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * 
- * @author Júlio Mendes
- *
- */
+import br.com.juliomendes90.cadastrofornecedor.models.Fornecedor;
+import br.com.juliomendes90.cadastrofornecedor.repository.FornecedorRepository;
+
 @Controller
 public class FornecedorController {
 
-	@RequestMapping("/cadastrarFornecedor")
+	@Autowired
+	private FornecedorRepository repository; 
+	
+	@RequestMapping(value = "/cadastrarFornecedor", method = RequestMethod.GET)
 	public String form() {
 		return "fornecedor/formFornecedor";
+	}
+	
+	@RequestMapping(value = "/cadastrarFornecedor", method = RequestMethod.POST)
+	public String form(Fornecedor fornecedor) {
+		
+		this.repository.save(fornecedor);
+		
+		return "redirect:/cadastrarFornecedor";
+	}
+	
+	@RequestMapping(value = "/listarFornecedores")
+	public ModelAndView listarFornecedores() {
+		ModelAndView mv = new ModelAndView("/index");
+		Iterable<Fornecedor> listaFornecedores = this.repository.findAll();
+		mv.addObject("fornecedores", listaFornecedores);
+		
+		return mv;
 	}
 }
---
+++ src/main/java/br/com/juliomendes90/cadastrofornecedor/repository/FornecedorRepository.java
@@ -0,0 +1,8 @@
+package br.com.juliomendes90.cadastrofornecedor.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.juliomendes90.cadastrofornecedor.models.Fornecedor;
+
+public interface FornecedorRepository extends CrudRepository<Fornecedor, String> {
+}
